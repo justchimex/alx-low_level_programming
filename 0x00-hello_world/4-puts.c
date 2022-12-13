1afb99ceb1abf0de01179376ca6e2e6226880bc5
Bright chimezie <just4chimex@gmail.com>
2022-12-12 16:30:01 -0800

    hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *          
- * main - main block                                          
+ * main - Entry
  *
- * Return: 0   
- */                                                                          
-int main(void)  /*function declaration*/                                                                
-{                
-	puts("\"Programming is like building a multilingual puzzle");      	        return (0);
+ * Return: Always 0
+ */
+int main (void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
